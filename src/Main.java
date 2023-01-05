3bd6d9aebb79f60d8b091808332b407caea9c89f
NaatLi <naat.li@yandex.ru>
2023-01-05 19:13:56 +0300

    Добавление класса Main, с демонстрацией работы класса SalesManager

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] sales = {10, 45, 56};
+
+        SalesManager salesManager = new SalesManager(sales);
+
+        int bestSales = salesManager.max();
+
+        System.out.println(bestSales);
+
+
+
+    }
+}
